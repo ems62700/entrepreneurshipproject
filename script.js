6c4c07bde3ecf92b8187a836ea0060a3b565dc0d
ems62700 <emarshall@cristoreyny.org>
2017-04-27 12:10:59 +0000

    Save changes

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
     $("#go").click(function() {
-        var name = $("#search").val();
-    var part1= "https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?api_key=fpFzrGNVZj3l0b34GUvU4gIm34pnT13u6EsUsF1U&location=Denver+CO";
+        //var name = $("#search").val();
+        //var part1 = "https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?api_key=fpFzrGNVZj3l0b34GUvU4gIm34pnT13u6EsUsF1U&location=Denver+CO";
+        $.getJSON("api.questions.json",
+        function (response){
+          console.log(response)  
+        })
+    })
+})
